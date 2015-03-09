020456faa167632a097d0d1c0e56bfc73bf0ec14
Tuttle26 <irunji00@fesb.hr>
2015-03-09 10:43:00 +0100

    commi55

--- mojapp/Program.cs
+++ mojapp/Program.cs
@@ -10,6 +10,7 @@ namespace mojapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World\n");
+            Console.WriteLine("Hello World2\n");
         }
     }
 }
